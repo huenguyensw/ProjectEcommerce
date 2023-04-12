6f4a6f578b1860912871ac970027a5b8a6ae970a
Nadya Khan <nadyakhan24@live.com>
2023-04-12 11:40:23 +0200

    link to createproduct.jsx

--- vite-project/src/pages/admin/ManageProducts.jsx
+++ vite-project/src/pages/admin/ManageProducts.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const ManageProducts = () => {
   return (
     <div>
-      
+      <Link to='/admin/create-product'>Create Product</Link>
     </div>
   )
 }
