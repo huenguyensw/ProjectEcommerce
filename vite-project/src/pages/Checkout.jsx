889d5060a8fbcd743eb2a4ca4e11c4bdf1481c3f
huenguyensw <hue.blue@gmail.com>
2023-04-14 00:46:11 +0200

    commit

--- vite-project/src/pages/Checkout.jsx
+++ vite-project/src/pages/Checkout.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import Cart from '../components/Cart'
 
 const Checkout = () => {
   return (
     <div>
-      <Cart />
+        <section className='orders'>
+            <h3>Products</h3>
+            <hr/>
+            <Cart/>
+        </section>
+        <section className='news'>
+            <h3>Newsletter</h3>
+            <hr/>
+            <input type='text'></input>
+            <button className='subscribe'>Subscribe</button>
+        </section>
+        <section className='shipping'>
+            <h3>Shipping address</h3>
+            <hr/>
+            <label >
+                First name
+                <br/>
+                <input type='text'></input>
+            </label>
+            <label>
+                Last name
+                <br/>
+                <input type='text'></input>
+            </label>
+            <label>
+                Email
+                <br/>
+                <input type='text'></input>
+            </label>
+            <label>
+                Password
+                <br/>
+                <input type='text'></input>
+            </label>
+            <label>
+                Country
+                <br/>
+                <select>
+                    <option selected>Sweden</option>
+                    <option>Finland</option>
+                    <option>Denmark</option>
+                    <option>Norway</option>
+                </select>
+            </label>
+            <label>
+                Comment
+                <br/>
+                <textarea></textarea>
+            </label>
+            <button>Proceed to payment</button>
+        </section>
+      
     </div>
   )
 }
