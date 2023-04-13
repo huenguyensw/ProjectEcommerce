beb8009783f8691edf2413f83f8af1dbb39fb2b7
Malin Schoening <malin.schoening@gmail.com>
2023-04-13 19:58:08 +0200

    commit

--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Cart = () => {
   return (
-    <div id="cart">
-      Here is the mini cart! 
-    </div>
+    <motion.div id="cart">
+      Here is the mini cart!
+    </motion.div>
   )
 }
 
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import Navigation from './Navigation'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 import Cart from './Cart'
 
 const Header = () => {
-    const showCart = () => {
-    }
+    const [cart, setCart] = useState(false)
 
+    const toggle = () => {
+        setCart(!cart)
+        console.log(cart)
+    }
 
     return (
         <div className='header'>
             <Navigation />
             <section>
-                <button onClick={showCart}><FontAwesomeIcon icon={faCartShopping}/></button>
+                <button onClick={toggle}><FontAwesomeIcon icon={faCartShopping}/></button>
+                {cart ? <Cart />
+                : ''}
             </section>
         </div>
     )
